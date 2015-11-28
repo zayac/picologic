7d99101500a190cff2cb9fcf472afb68162bf594
Pavel Zaichenkov <zaichenkov@gmail.com>
2015-11-28 19:15:22 +0000

    adding documention

--- src/logic.mli
+++ src/logic.mli
@@ -17,22 +17,29 @@ val sexp_of_t : t -> Sexplib.Sexp.t
 
 include Comparable.S with type t := t
 
-(** {1 Logical operators } *)
+(** {2 Logical operators } *)
 
 (** Disjunction *)
 val ( + ) : t -> t -> t
+
 (** Conjunction *)
 val ( * ) : t -> t -> t
+
 (** Negation *)
 val ( ~- ) : t -> t
+
 (** Implication *)
 val ( ==> ) : t -> t -> t
+
 (** Converse implication *)
 val ( <== ) : t -> t -> t
+
 (** Equivalence *)
 val ( <=> ) : t -> t -> t
+
 (** Disjunction *)
 val sum : t list -> t
+
 (** Conjunction *)
 val product: t list -> t
 
@@ -42,18 +49,22 @@ val product: t list -> t
 (** Converts the string to an expression.  Returns [None] if the string is
     ill-formed.  The function is right-associative. *)
 val from_string : string -> t option
+
 (** Converts the expression to a string *)
 val to_string : t -> string
+
 (** Converts the expression to a string with logic unicode characters.  *)
 val to_pretty_string : t -> string
 
 (** [is_ground t] returns [true] if the logical expression does not contain
     variables and [false] otherwise *)
 val is_ground : t -> bool
+
 (** [evaluate m t] substitutes variables in [t] with values from [m] and
     evaluates the expression.  If there exist variables in [t] that are not
     present in [m], [None] is returned. *)
 val evaluate : bool String.Map.t -> t -> bool option
+
 (** Reduces and simplifies the expression *)
 val simplify : t -> t
 
--- src/sat.mli
+++ src/sat.mli
@@ -1,16 +1,43 @@
+(** SAT solver *)
 
 
+(** An interface for a DIMACS SAT solver.  The interface corresponds to the
+    PicoSAT solver, however, other existing solvers should have a similar
+    interface. *)
 module type Solvable = sig
+  (** The solver data structure *)
   type solver
+
+  (** Constructor *)
   val init : unit -> solver
+
+  (** Adds a literal of the next clause.  A zero terminates the clause. *)
   val add : solver -> int -> int
+
+  (** Call the main SAT routine.  An integer argument sets a limit on the number
+      of decisions.  A negative decision sets no limit.  [sat] returns 10 if
+      the constraints are [SATISFIABLE], 20 if the constraints are
+      [UNSATISFIABLE] and other integers if the solution is [UNKNOWN]. *)
   val sat : solver -> int -> int
+
+(** After [sat] was called and returned [SATISFIABLE], then the satisfying
+    assignment can be obtained by 'dereferencing' literals.  The return value is
+    0 for an unknown value. *)
   val deref : solver -> int -> int
 end
 
+(** A module that finds solutions to a SAT problem implemented as a functor. *)
 module Solver :
   functor (S : Solvable) ->
   sig
+    (** [solve l v c] returns a solution to the SAT probem [c], where [l] is
+        a limit on the number of decisions and [v] is a number of variables.
+        The function returns an empty list if the problem is UNSAT and [None] if
+        the solution is unknown.  If the solution is found, the outer list
+        always contains one element (list). *)
     val solve : ?limit:int -> int -> int list list -> int list list option
+
+  (** [solve_all l v c] finds all solutions to the SAT problem if one exists.
+      The interface is the same as for the function [solve]. *)
     val solve_all : ?limit:int -> int -> int list list -> int list list option
   end
