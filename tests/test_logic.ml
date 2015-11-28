129380b598c7417138c7d0909ba2406074ae904c
Pavel Zaichenkov <zaichenkov@gmail.com>
2015-11-28 18:48:29 +0000

    'tests' directory

---
+++ tests/test_logic.ml
@@ -0,0 +1,45 @@
+
+open OUnit2
+
+(* Test that logical operators work *)
+let test_ops _ =
+  let open Logic in
+  assert_equal
+    ((Var "x") + ~-(Var "x") * (Var "y") + (True * False))
+    (Or (Or (Var "x", And (Not (Var "x"), Var "y")), And (True, False)));
+  assert_equal (Var "x" ==> (Var "y")) (Or (Not (Var "x"), Var "y"));
+  assert_equal
+    (Var "x" <=> (Var "y"))
+    (Or (And (Var "x", Var "y"), And (Not (Var "x"), Not (Var "y"))));
+  assert_equal
+    (sum [Var "x"; True; Var "y"; False; Var "z"])
+    (Or (Var "z", Or (False, Or (Var "y", Or (True, Var "x")))));
+  assert_equal
+    (product [Var "x"; True; Var "y"; False; Var "z"])
+    (And (Var "z", And (False, And (Var "y", And (True, Var "x")))));
+  assert_equal (sum []) False;
+  assert_equal (product []) True
+
+(* Test that string conversions work *)
+let test_string_conv _ =
+  let open Core.Std in
+  let open Logic in
+  let e = from_string "x + ~x * y + (1 * 0)" in
+  assert_equal e
+    (Some (Or (Var "x", Not (And (Var "x", Or (Var "y", And (True, False)))))));
+
+  assert_equal
+    (to_string (Or (Var "x", Not (And (Var "x", Or (Var "y", And (True, False)))))))
+    "(x + ~(x * (y + (1 * 0))))";
+
+  assert_equal
+    (to_pretty_string (Or (Var "x", Not (And (Var "x", Or (Var "y", And (True, False)))))))
+    "(x ∨ ¬(x ∧ (y ∨ (1 ∧ 0))))"
+
+let suite = "Test Logic" >::: [
+    "Test logical operators"   >:: test_ops;
+    "Test string conversions"  >:: test_string_conv;
+  ]
+
+let _ =
+  run_test_tt_main suite
