addfa63ba4d7e11c20e239586c59022eb4198421
Pavel Zaichenkov <zaichenkov@gmail.com>
2015-12-02 01:26:07 +0000

    modified cnf structure

--- src/cnf.ml
+++ src/cnf.ml
@@ -5,9 +5,10 @@ module CSet = Set.Poly
 
 module Atom = struct
   module T = struct
-    type t =
-    | AVar of string
-    | ANegVar of string
+    type t = {
+      var: string;
+      is_neg: bool
+    }
     with sexp, compare
   end
   include T
@@ -32,19 +33,18 @@ let ( + ) t t' =
 
 let ( * ) = CSet.union
 
+let neg_atom t = Atom.{ t with is_neg = not (t.is_neg) }
+
 let ( ~- ) t =
   let open Atom in
-  let neg = function
-    | AVar s -> ANegVar s
-    | ANegVar s -> AVar s in
   let lst = ref [] in
   CSet.iter t
     ~f:(fun disj ->
       let tmp = ref !lst in
       lst := Atom.Set.fold disj ~init:[]
         ~f:(fun acc el ->
-          if List.is_empty !tmp then [neg el] :: acc
-          else (List.map !tmp ~f:(fun l -> (neg el) :: l)) @ acc
+          if List.is_empty !tmp then [neg_atom el] :: acc
+          else (List.map !tmp ~f:(fun l -> (neg_atom el) :: l)) @ acc
         );
     );
   CSet.of_list (List.map ~f:Atom.Set.of_list !lst) 
@@ -59,7 +59,7 @@ let xor t t' = (t * ~-t') + (~-t * t')
 
 let mk_false = CSet.singleton Atom.Set.empty
 let mk_true = CSet.empty
-let mk_var s = CSet.singleton Atom.(Set.singleton (AVar s))
+let mk_var s = CSet.singleton Atom.(Set.singleton { var = s; is_neg = false; })
 
 let rec sum = function
   | [] -> mk_false
@@ -72,11 +72,9 @@ let rec product = function
   | hd :: tl -> hd * (product tl)
 
 let rec to_string_helper ?(sand="*") ?(sor="+") ?(snot="~") t =
-  let print t =
-    let open Atom in
-    match t with
-    | ANegVar s -> Printf.sprintf "%s%s" snot s
-    | AVar s -> s in
+  let open Atom in
+  let print { var = s; is_neg = b; } =
+    if b then Printf.sprintf "%s%s" snot s else s in
   let t = CSet.to_list t in
   let t =
     List.map t ~f:(fun s -> List.map (Atom.Set.to_list s) ~f:print) in
@@ -92,8 +90,7 @@ let to_pretty_string = to_string_helper ~sand:"∧" ~sor:"∨" ~snot:"¬"
 
 let vars t =
   let open Atom in
-  let add acc = function
-  | AVar s | ANegVar s -> String.Set.add acc s in
+  let add acc { var = s; is_neg = _; } = String.Set.add acc s in
   CSet.fold t ~init:String.Set.empty
     ~f:(fun acc el -> Atom.Set.fold ~init:acc ~f:add el)
 
@@ -107,9 +104,7 @@ let from_logic t =
       ~f:(fun acc el ->
         let open Atom in
         match Int.Map.find map Int.(if el > 0 then el else ~-el) with
-        | Some v ->
-          if Int.(el > 0) then Atom.Set.add acc (AVar v)
-          else Atom.Set.add acc (ANegVar v)
+        | Some v -> Atom.Set.add acc { var = v; is_neg = Int.(el < 0); }
         | None ->
           raise (Failure "'to_dimacs' function from Logic module went wrong")
       ) in
@@ -119,8 +114,8 @@ let to_logic =
   let open Atom in
   let open Logic in
   let atom_to_var = function
-    | AVar s -> Var s
-    | ANegVar s -> (Not (Var s)) in
+    | { var = s; is_neg = false } -> Var s
+    | { var = s; is_neg = true } -> (Not (Var s)) in
   let set_to_logic =
     Atom.Set.fold ~init:False
       ~f:(fun acc el ->
@@ -140,8 +135,8 @@ let from_string t =
 let evaluate map =
   let open Atom in
   let substitute map = function
-    | AVar s -> String.Map.find map s
-    | ANegVar s -> Option.map ~f:not (String.Map.find map s) in
+    | { var = s; is_neg = false; } -> String.Map.find map s
+    | { var = s; is_neg = true; } -> Option.map ~f:not (String.Map.find map s) in
   let select f acc el =
     match acc, f map el with
     | (None, _) | (_, None) -> None
@@ -149,14 +144,6 @@ let evaluate map =
   let eval_set _ =
     Atom.Set.fold ~init:(Some false) ~f:(select substitute) in
   CSet.fold ~init:(Some true) ~f:(select eval_set)
-
-(** Gets a name of a variable from an expression of type [Atom] *)
-let get_string = function
-  | Atom.AVar s | Atom.ANegVar s -> s
-
-let neg_atom = function
-  | Atom.AVar s -> Atom.ANegVar s
-  | Atom.ANegVar s -> Atom.AVar s
 
 let simplify =
   let simplify_disj =
